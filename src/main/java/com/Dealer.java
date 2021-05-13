c8e1980618dab3a2c0994ee69ecb2bc00fe2ecc6
Mitsuru Takeda <mitsuru.takeda03@gmail.com>
2021-05-13 18:30:02 +0900

    dealer can't stay under 17

--- src/main/java/com/Dealer.java
+++ src/main/java/com/Dealer.java
@@ -1,12 +1,27 @@
 package com;
 
+import java.util.Scanner;
+
 /**
  * playerクラスを継承
  * カードの合計が16以下では降りれない
  */
 public class Dealer extends Player{
     /**
-     * TODO
      * isContinueに16以下では降りれないようにする機能を追加
      */
+    @Override
+    public boolean isContinue(){
+        checkCards();
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+        if(inputLine.equals("yes") || inputLine.equals("Yes") || inputLine.equals("YES"))
+            return true;
+        else if(checkSum() < 17 && checkSum() > 0){
+            System.out.println("Dealer can't stay under 17");
+            return true;
+        }
+        else
+            return false;
+    }
 }
